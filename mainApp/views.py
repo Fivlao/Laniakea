10bb92b95faffe44b302b34e5b8dc5906d5ecb57
fivlao <oleh6234@gmail.com>
2019-02-27 23:49:14 +0200

    1.01

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('mainApp/html/main.html')
+    return render(request, 'mainsDiv/html/main.html')
